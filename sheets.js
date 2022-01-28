80e62cb8258bc4e3aa176e29cc316d215b7c7787
Chuck Konkol <ckonkol@gmail.com>
2022-01-28 12:02:08 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
     var geturllistvalue = get("list");
+     alert(geturllistvalue);
 var jsonData = jQuery.getJSON(geturllistvalue);
-
 jsonData.then(function(data, textStatus, jqxhr) {
   alert(jqxhr.responseJSON)
 })

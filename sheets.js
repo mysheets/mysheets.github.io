88403fe371661c02413889943be3a0a5dcdae8ec
Chuck Konkol <ckonkol@gmail.com>
2022-01-28 11:00:40 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,24 +1,17 @@
 window.onload = function() {
     alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
-   loadJSON("https://jsonplaceholder.typicode.com/posts", myData,'jsonp');
+    var geturllistvalue = get("list");
+    async function get() {
+    let url = geturllistvalue;
+    let obj = await (await fetch(url)).json();
+    
+    //console.log(obj);
+    return obj;
+}
+var tags;
+(async () => {
+  tags = await get()
+  //console.log(tags)
+  document.getElementById("tags").innerHTML = JSON.stringify(tags);
+})()
 };
-
-function myData(Data)
-{
-   alert(Data);
-}
-function loadJSON(path, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        success(JSON.parse(xhr.responseText));
-      }
-      else {
-        error(xhr);
-      }
-    }
-  };
-  xhr.open('GET', path, true);
-  xhr.send();
-}

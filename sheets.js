1269eb021aaa515db6ec76f87227d27b5d97662b
Chuck Konkol <ckonkol@gmail.com>
2022-01-28 10:52:06 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -7,3 +7,18 @@ function myData(Data)
 {
    alert(Data);
 }
+function loadJSON(path, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        success(JSON.parse(xhr.responseText));
+      }
+      else {
+        error(xhr);
+      }
+    }
+  };
+  xhr.open('GET', path, true);
+  xhr.send();
+}

45b0fe04e4c633014b93e4c8ab491d1774a332f9
Chuck Konkol <ckonkol@gmail.com>
2022-01-28 10:17:20 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,12 +1,3 @@
- //v 4.1 get URL
-var getsheet = get("list");
-    if (geturllistvalue) {
-     
-      shoppinglist = geturllistvalue;
-  }else if (y){
-       y = y.split('%2C');
-      shoppinglist = y;
-  }
 $(document).ready(function(){
  var getsheet = get("list");
     $.getJSON(getsheet, function(result){

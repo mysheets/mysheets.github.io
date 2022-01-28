8d0cc400e97a538b79f4904b409425aae1139b12
Chuck Konkol <ckonkol@gmail.com>
2022-01-28 11:40:09 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,17 +1,12 @@
 window.onload = function() {
     alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
     var geturllistvalue = get("list");
-    async function get() {
-    let url = geturllistvalue;
-    let obj = await (await fetch(url)).json();
-    
-    //console.log(obj);
-    return obj;
-}
-var tags;
-(async () => {
-  tags = await get()
-  //console.log(tags)
-  document.getElementById("tags").innerHTML = JSON.stringify(tags);
-})()
+    // Make a request for a user with a given ID
+axios.get(geturllistvalue)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 };

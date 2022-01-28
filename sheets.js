88172ae590bc699549a9dffb98178cf8bcd39768
Chuck Konkol <ckonkol@gmail.com>
2022-01-28 10:22:04 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
- var getsheet = get("list");
+window.onload = function() {
+    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    var getsheet = get("list");
     $.getJSON(getsheet, function(result){
       $.each(result, function(i, field){
         $("div").append(field + " ");
       });
     });
   });
-});
+};
